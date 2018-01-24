4e04181d618a5780dcce67e862e9f618d218568c
Adam Jarzebak <adam@jarzeabak.eu>
2018-01-24 08:33:24 +0000

    Add another version of client server communication for opencv and camera transmition

---
+++ webStream/ver1/server.py
@@ -0,0 +1,69 @@
+import socket
+import sys
+import cv2
+import pickle
+import numpy as np
+
+HOST = ''
+PORT = 8091
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+print('Socket created')
+
+s.bind((HOST,PORT))
+print('Socket bind complete')
+s.listen(10)
+print('Socket now listening')
+
+conn, addr=s.accept()
+
+data = []
+
+
+while True:
+    try:
+
+        packet = conn.recv(80)
+
+        if not packet:
+            break
+
+        data.append(packet)
+
+        data_e = pickle.loads(packet)
+
+        print(data_e)
+
+
+    except KeyboardInterrupt:
+        s.close()
+        print("Close connection")
+        break
+
+
+
+data_arr = pickle.loads(b"".join(data))
+    # data_arr = pickle.loads(packet)
+
+print(data_arr)
+
+# print(sys.getsizeof(data))
+
+data_arr = pickle.loads(data)
+
+
+frame = pickle.loads(data)
+
+    # print(frame)
+
+cv2.imshow('frame',frame)
+# s.close()
+
+    # data = []
+    # while True:
+    #     packet = s.recv(4096)
+        # if not packet: break
+        # data.append(packet)
+    # data_arr = pickle.loads(b"".join(data))
+    # print(data_arr)
+    # s.close()
